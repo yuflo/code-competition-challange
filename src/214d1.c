4500fbcc0201dbe078b38c24509efc622f69149c
lee.C <lichen@leeCs-MBP.lan>
2019-01-03 19:34:17 +0800

    ..

--- src/214d1.c
+++ src/214d1.c
@@ -1,26 +1,29 @@
 #include "stdio.h"
 
+#define true (char)1
+#define false (char)0
+
 typedef char bool;
 
+int n = 4;
+int a[] = {1, 2, 4, 7};
+int k = 13;
+
+bool dfs(int i, int sum){
+  if(i == n) return sum == k;
+
+  if(dfs(i+1, sum)) return true;
+
+  if(dfs(i+1, sum+a[i])) return true;
+
+  return false;
+}
+
+bool dfsStack(){
+
+}
+
 int main(int argc, char *argv[]){
-  int n = 4;
-  int a[] = {1, 2, 4, 7};
-  int k = 13;
-
-  // solve by `dfs` strategy
-  int stk[n]; // c99
-  int sp = -1;
-  int v = 0;
-  int status[n]; // c99
-
-  //for(int i=0; i < n; i++){
-  while(true){
-    if(status[i] == 2) --sp;
-    else if(status[i] == 0){
-      if(chain(status, a, sp) > k){
-        status[i] = 0;
-      }
-    }
-    else if(status[i] == 1);
-  }
+  if(dfs(0, 0)) printf("yes\n");
+  else printf("no\n");
 }
